4290cfd53af114d93672802ec67f7321f40c62d1
denis <denisicun@gmail.com>
2017-10-20 08:27:35 +0300

    test reaact stuf and config webpack

---
+++ public/src/components/header/header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Logo from './logo/logo';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Logo />
+      </div>
+    );
+  }
+}
---
+++ public/src/components/header/logo/logo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './logoS.css';
+// ss
+export default class Logo extends Component {
+  render() {
+    return (
+      <div className='logo'>
+        <h1 className='logopic'>MeetMe</h1>
+      </div>
+    );
+  }
+}
